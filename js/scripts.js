6e7ddf19e269d81d0b369cd5052ef87bd904bed2
Mikhail Shishov <contact@michaelshishov.com>
2022-09-01 10:09:58 +0200

    Header and home page fixes

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,36 @@
+// header class on scroll
+let scrollpos = window.scrollY;
+function add_class_on_scroll() {
+    document.body.classList.add("fixed-header");
+}
+function remove_class_on_scroll() {
+    document.body.classList.remove("fixed-header");
+}
+window.addEventListener("scroll", function () {
+    scrollpos = window.scrollY;
+    if (scrollpos > 40) {
+        add_class_on_scroll();
+    } else {
+        remove_class_on_scroll();
+    }
+});
+
+// header dropdown
+document.querySelector(".account-dropdown").addEventListener("click", function () {
+    this.classList.toggle("active");
+})
+
 // pricing open full list
 const pricingLinks = document.querySelectorAll('.card-price-list-show');
 
 for (const pricingLink of pricingLinks) {
-    pricingLink.addEventListener('click', function handleClick(e) {
+    pricingLink.addEventListener("click", function handleClick(e) {
         e.preventDefault();
-        pricingLink.classList.add('open');
+        pricingLink.classList.add("open");
     });
 }
 
 // pricing markets
-// const priceInput = document.getElementById("input-value");
-// const priceOutput = document.getElementById("output-value");
-
-// priceInput.addEventListener('change', (event) => {
-//     priceOutput.value = parseFloat(this.value);
-//     // const result = parseFloat(this.value);
-//     // console.log(result);
-// });
-
 function sliderChange(val) {
     document.getElementById("output-value").value = val * 8.99;
     document.querySelector(".range-input-value").textContent = document.getElementById("input-value").value;
