f212e62fac693f715a98edd9609ca84e0e1e6509
Mikhail Shishov <contact@michaelshishov.com>
2022-09-07 08:58:54 +0200

    Trello bug fixes

--- js/scripts.js
+++ js/scripts.js
@@ -1,123 +1,135 @@
 // header class on scroll
 let scrollpos = window.scrollY;
 function add_class_on_scroll() {
-    document.body.classList.add("fixed-header");
+  document.body.classList.add("fixed-header");
 }
 function remove_class_on_scroll() {
-    document.body.classList.remove("fixed-header");
+  document.body.classList.remove("fixed-header");
 }
 window.addEventListener("scroll", function () {
-    scrollpos = window.scrollY;
-    if (scrollpos > 40) {
-        add_class_on_scroll();
-    } else {
-        remove_class_on_scroll();
-    }
+  scrollpos = window.scrollY;
+  if (scrollpos > 40) {
+    add_class_on_scroll();
+  } else {
+    remove_class_on_scroll();
+  }
 });
 
 // header dropdown
-document.querySelector(".account-dropdown")?.addEventListener("click", function () {
+document
+  .querySelector(".account-dropdown")
+  ?.addEventListener("click", function () {
     this.classList.toggle("active");
-})
+  });
 
 // pricing open full list
 const pricingLinks = document.querySelectorAll(".card-price-list-show");
 
 for (const pricingLink of pricingLinks) {
-    pricingLink.addEventListener("click", function handleClick(e) {
-        e.preventDefault();
-        pricingLink.classList.add("open");
-    });
+  pricingLink.addEventListener("click", function handleClick(e) {
+    e.preventDefault();
+    pricingLink.classList.add("open");
+  });
 }
 
 // pricing markets
 function sliderChange(val) {
-    document.getElementById("output-value").value = val * 8.99;
-    document.querySelector(".range-input-value").textContent = document.getElementById("input-value").value;
-    document.getElementById("output-value").style.width = (document.getElementById("output-value").value.length - 0.8) + 'ch';
+  document.getElementById("output-value").value = val * 8.99;
+  document.querySelector(".range-input-value").textContent =
+    document.getElementById("input-value").value;
+  document.getElementById("output-value").style.width =
+    document.getElementById("output-value").value.length - 0.8 + "ch";
 }
 
 // tabs
 function Tabs() {
-    var bindAll = function () {
-        var menuElements = document.querySelectorAll("[data-tab]");
-        for (var i = 0; i < menuElements.length; i++) {
-            menuElements[i].addEventListener("click", change, false);
-        }
-    };
+  var bindAll = function () {
+    var menuElements = document.querySelectorAll("[data-tab]");
+    for (var i = 0; i < menuElements.length; i++) {
+      menuElements[i].addEventListener("click", change, false);
+    }
+  };
 
-    var clear = function () {
-        var menuElements = document.querySelectorAll("[data-tab]");
-        for (var i = 0; i < menuElements.length; i++) {
-            menuElements[i].classList.remove("active");
-            var id = menuElements[i].getAttribute("data-tab");
-            document.getElementById(id).classList.remove("active");
-        }
-    };
+  var clear = function () {
+    var menuElements = document.querySelectorAll("[data-tab]");
+    for (var i = 0; i < menuElements.length; i++) {
+      menuElements[i].classList.remove("active");
+      var id = menuElements[i].getAttribute("data-tab");
+      document.getElementById(id).classList.remove("active");
+    }
+  };
 
-    var change = function (e) {
-        clear();
-        e.target.classList.add("active");
-        var id = e.currentTarget.getAttribute("data-tab");
-        document.getElementById(id).classList.add("active");
-    };
+  var change = function (e) {
+    clear();
+    e.target.classList.add("active");
+    var id = e.currentTarget.getAttribute("data-tab");
+    document.getElementById(id).classList.add("active");
+  };
 
-    bindAll();
+  bindAll();
 }
 var connectTabs = new Tabs();
 
 // annual/month pricing switch
 const switchPrice = document.querySelector(".form-pricing-switch");
-const togglePrice = () => document.querySelectorAll(".shop-choice-item")
-    .forEach(label => label.classList.toggle("disabled"))
-
-switchPrice.addEventListener("change", togglePrice);
+const togglePrice = () =>
+  document
+    .querySelectorAll(".shop-choice-item")
+    .forEach((label) => label.classList.toggle("disabled"));
+if (switchPrice) {
+  switchPrice.addEventListener("change", togglePrice);
+}
 const shopRadios = document.querySelectorAll(".shop-choice-item-radio");
-document.querySelector(".form-pricing-switch").addEventListener("click", function () {
+document
+  .querySelector(".form-pricing-switch")
+  ?.addEventListener("click", function () {
     console.log("switch");
     for (const shopRadio of shopRadios) {
-        switchPrice.checked ? shopRadio.disabled = true : shopRadio.disabled = false;
+      switchPrice.checked
+        ? (shopRadio.disabled = true)
+        : (shopRadio.disabled = false);
     }
-});
+  });
 
 // file upload
-document.querySelector("html").classList.add('js');
+document.querySelector("html").classList.add("js");
 
-var fileInput = document.querySelector(".form-input-file"),
-    button = document.querySelector(".form-input-file-trigger"),
-    the_return = document.querySelector(".form-file-return");
-
-button.addEventListener("keydown", function (event) {
+let fileInput = document.querySelector(".form-input-file"),
+  button = document.querySelector(".form-input-file-trigger"),
+  the_return = document.querySelector(".form-file-return");
+if (fileInput) {
+  button.addEventListener("keydown", function (event) {
     if (event.keyCode == 13 || event.keyCode == 32) {
-        fileInput.focus();
+      fileInput.focus();
     }
-});
-button.addEventListener("click", function (event) {
+  });
+  button.addEventListener("click", function (event) {
     fileInput.focus();
     return false;
-});
-fileInput.addEventListener("change", function (event) {
+  });
+  fileInput.addEventListener("change", function (event) {
     the_return.innerHTML = this.files[0].name;
-});  
+  });
+}
 
 // select
-const select = document.querySelectorAll('.select-btn');
-const option = document.querySelectorAll('.option');
+const select = document.querySelectorAll(".select-btn");
+const option = document.querySelectorAll(".option");
 let index = 1;
 
-select.forEach(a => {
-	a.addEventListener('click', b => {
-		const next = b.target.nextElementSibling;
-		next.classList.toggle('toggle');
-		next.style.zIndex = index++;
-	})
-})
-option.forEach(a => {
-	a.addEventListener('click', b => {
-		b.target.parentElement.classList.remove('toggle');
-		
-		const parent = b.target.closest('.select').children[0];
-		parent.setAttribute('data-type', b.target.getAttribute('data-type'));
-		parent.innerText = b.target.innerText;
-	})
-})
+select.forEach((a) => {
+  a.addEventListener("click", (b) => {
+    const next = b.target.nextElementSibling;
+    next.classList.toggle("toggle");
+    next.style.zIndex = index++;
+  });
+});
+option.forEach((a) => {
+  a.addEventListener("click", (b) => {
+    b.target.parentElement.classList.remove("toggle");
+
+    const parent = b.target.closest(".select").children[0];
+    parent.setAttribute("data-type", b.target.getAttribute("data-type"));
+    parent.innerText = b.target.innerText;
+  });
+});
